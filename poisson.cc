8ebf344e0b3620308d3f1bd67f1ed22d456e562f
tappelnano <102976128+tappelnano@users.noreply.github.com>
2022-05-16 15:27:44 +0000

    blatt 3

--- poisson.cc
+++ poisson.cc
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
-double poisson(double mu, int k) {
-    return 0;
+double poisson(double mu, int k) {  
+  return pow(mu, k)*(exp(-mu))/(tgamma(k+1));
 }
+
+double c = poisson(1, 2);
 
 int main() {
     using namespace std;
 
 
     ifstream fin("datensumme.txt");
+    ofstream fout("hist.txt");
     int n_i;
+    vector <int> zaehler(11);
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i] += 1;
     }
+    
     fin.close();
+  for ( unsigned int k = 0 ; k < zaehler . size () ; ++ k ) {
+  std :: cout << k << " : " << zaehler [ k ] << std :: endl ;
+  fout << k << " " << zaehler [ k ] << " " << 234*(poisson(3.11538, k)) << std :: endl;
+}
 }
